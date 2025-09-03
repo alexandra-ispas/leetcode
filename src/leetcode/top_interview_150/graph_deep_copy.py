aa06645e68a21f411cd66c0b77c73ee0fc794991
alexandra-ispas <aispas33@gmail.com>
2025-09-03 20:18:37 +0300

    more+++

---
+++ src/leetcode/top_interview_150/graph_deep_copy.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):
+    def __init__(self):
+        self.mapping = {}
+
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return None
+
+        if node in self.mapping:
+            return self.mapping[node]
+
+        new_node = Node(val=node.val)
+        self.mapping[node] = new_node
+
+        for neigh in node.neighbors:
+            new_node.neighbors.append(self.cloneGraph(neigh))
+
+        return new_node
+
