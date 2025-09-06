536b9d7562a7974e8be2dd2f06db672555aba94f
alexandra-ispas <aispas33@gmail.com>
2025-09-06 21:40:31 +0300

    more

---
+++ src/leetcode/top_interview_150/inorder.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        result = []
+
+        def order(node):
+            if node is None:
+                return
+
+            order(node.left)
+            result.append(node.val)
+            order(node.right)
+
+        order(root)
+        return result
