69c16cf97d0efb28cd700b3e51714770bc2f45ca
alexandra-ispas <aispas33@gmail.com>
2025-09-09 14:08:23 +0300

    more

---
+++ src/leetcode/top_interview_150/read_write.py
@@ -0,0 +1,42 @@
+import json
+import random
+import uuid
+
+
+class Person(object):
+    name: str
+    age: int
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+
+    def to_json(self) -> str:
+        return json.dumps(self.__dict__)
+
+
+def write():
+    persons = [
+        Person(name=str(uuid.uuid1()), age=random.randint(0, 50)) for _ in range(10)
+    ]
+    # create a file
+    file = "persons.txt"
+    with open(file, "w") as f:
+        for p in persons:
+            f.write(p.to_json() + "\n")
+
+
+def read():
+    input_file = "persons.txt"
+    with open(input_file, "r") as f:
+        for line in f:
+            print(line)
+            print(Person(**dict(json.loads(line.strip()))))
+
+
+def main():
+    read()
+
+
+if __name__ == "__main__":
+    main()
